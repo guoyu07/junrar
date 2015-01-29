9889902450191394b295b6aad1f378ae4585174a
Nikolay Pultsin <geometer@fbreader.org>
2015-01-29 19:23:43 +0000

    removed useless getters/setter (usage of fields directly makes it faster on Android L)

--- unrar/src/main/java/com/github/junrar/unpack/Unpack20.java
+++ unrar/src/main/java/com/github/junrar/unpack/Unpack20.java
@@ -233,58 +233,58 @@ public abstract class Unpack20 extends Unpack15
 
 		Arrays.fill(lenCount, 0);// memset(LenCount,0,sizeof(LenCount));
 
-		Arrays.fill(dec.getDecodeNum(), 0);// memset(Dec->DecodeNum,0,Size*sizeof(*Dec->DecodeNum));
+		Arrays.fill(dec.decodeNum, 0);// memset(Dec->DecodeNum,0,Size*sizeof(*Dec->DecodeNum));
 
 		for (i = 0; i < size; i++) {
 			lenCount[(int) (lenTab[offset + i] & 0xF)]++;
 		}
 		lenCount[0] = 0;
-		for (tmpPos[0] = 0, dec.getDecodePos()[0] = 0, dec.getDecodeLen()[0] = 0, N = 0, i = 1; i < 16; i++) {
+		for (tmpPos[0] = 0, dec.decodePos[0] = 0, dec.decodeLen[0] = 0, N = 0, i = 1; i < 16; i++) {
 			N = 2 * (N + lenCount[i]);
 			M = N << (15 - i);
 			if (M > 0xFFFF) {
 				M = 0xFFFF;
 			}
-			dec.getDecodeLen()[i] = (int) M;
-			tmpPos[i] = dec.getDecodePos()[i] = dec.getDecodePos()[i - 1]
+			dec.decodeLen[i] = (int) M;
+			tmpPos[i] = dec.decodePos[i] = dec.decodePos[i - 1]
 					+ lenCount[i - 1];
 		}
 
 		for (i = 0; i < size; i++) {
 			if (lenTab[offset + i] != 0) {
-				dec.getDecodeNum()[tmpPos[lenTab[offset + i] & 0xF]++] = i;
+				dec.decodeNum[tmpPos[lenTab[offset + i] & 0xF]++] = i;
 			}
 		}
-		dec.setMaxNum(size);
+		dec.maxNum = size;
 	}
 
 	protected int decodeNumber(Decode dec)
 	{
 		int bits;
 		long bitField = getbits() & 0xfffe;
-//        if (bitField < dec.getDecodeLen()[8]) {
-//			if (bitField < dec.getDecodeLen()[4]) {
-//				if (bitField < dec.getDecodeLen()[2]) {
-//					if (bitField < dec.getDecodeLen()[1]) {
+//        if (bitField < dec.decodeLen[8]) {
+//			if (bitField < dec.decodeLen[4]) {
+//				if (bitField < dec.decodeLen[2]) {
+//					if (bitField < dec.decodeLen[1]) {
 //						bits = 1;
 //					} else {
 //						bits = 2;
 //					}
 //				} else {
-//					if (bitField < dec.getDecodeLen()[3]) {
+//					if (bitField < dec.decodeLen[3]) {
 //						bits = 3;
 //					} else {
 //						bits = 4;
 //					}
 //				}
 //			} else {
-//				if (bitField < dec.getDecodeLen()[6]) {
-//					if (bitField < dec.getDecodeLen()[5])
+//				if (bitField < dec.decodeLen[6]) {
+//					if (bitField < dec.decodeLen[5])
 //						bits = 5;
 //					else
 //						bits = 6;
 //				} else {
-//					if (bitField < dec.getDecodeLen()[7]) {
+//					if (bitField < dec.decodeLen[7]) {
 //						bits = 7;
 //					} else {
 //						bits = 8;
@@ -292,19 +292,19 @@ public abstract class Unpack20 extends Unpack15
 //				}
 //			}
 //		} else {
-//			if (bitField < dec.getDecodeLen()[12]) {
-//				if (bitField < dec.getDecodeLen()[10])
-//					if (bitField < dec.getDecodeLen()[9])
+//			if (bitField < dec.decodeLen[12]) {
+//				if (bitField < dec.decodeLen[10])
+//					if (bitField < dec.decodeLen[9])
 //						bits = 9;
 //					else
 //						bits = 10;
-//				else if (bitField < dec.getDecodeLen()[11])
+//				else if (bitField < dec.decodeLen[11])
 //					bits = 11;
 //				else
 //					bits = 12;
 //			} else {
-//				if (bitField < dec.getDecodeLen()[14]) {
-//					if (bitField < dec.getDecodeLen()[13]) {
+//				if (bitField < dec.decodeLen[14]) {
+//					if (bitField < dec.decodeLen[13]) {
 //						bits = 13;
 //					} else {
 //						bits = 14;
@@ -315,13 +315,13 @@ public abstract class Unpack20 extends Unpack15
 //			}
 //		}
 //		addbits(bits);
-//		int N = dec.getDecodePos()[bits]
-//				+ (((int) bitField - dec.getDecodeLen()[bits - 1]) >>> (16 - bits));
-//		if (N >= dec.getMaxNum()) {
+//		int N = dec.decodePos[bits]
+//				+ (((int) bitField - dec.decodeLen[bits - 1]) >>> (16 - bits));
+//		if (N >= dec.maxNum) {
 //			N = 0;
 //		}
-//		return (dec.getDecodeNum()[N]);
-        int[] decodeLen = dec.getDecodeLen();
+//		return (dec.decodeNum[N]);
+        int[] decodeLen = dec.decodeLen;
         if (bitField < decodeLen[8]) {
 			if (bitField < decodeLen[4]) {
 				if (bitField < decodeLen[2]) {
@@ -375,12 +375,12 @@ public abstract class Unpack20 extends Unpack15
 			}
 		}
 		addbits(bits);
-		int N = dec.getDecodePos()[bits]
+		int N = dec.decodePos[bits]
 				+ (((int) bitField - decodeLen[bits - 1]) >>> (16 - bits));
-		if (N >= dec.getMaxNum()) {
+		if (N >= dec.maxNum) {
 			N = 0;
 		}
-		return (dec.getDecodeNum()[N]);
+		return (dec.decodeNum[N]);
 	}
 
 	protected boolean ReadTables20() throws IOException, RarException
